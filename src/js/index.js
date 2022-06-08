e90da4c962f1e39e93e9c0317c3bec0dad155549
koechian <ianovic8@live.com>
2022-06-08 15:09:36 +0300

    Started on Cart Items

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+// Sign in and Login click events
+
 $(".register").hide();
 
 $("#register-button").click(function () {
@@ -13,10 +15,17 @@ $("#done").click(function () {
   $(".login").show();
 });
 
+// Landing click events
 $("#landing").click(function () {
   window.location.href = "/src/landing.html";
 });
+
+// Cart click and hover events
 $(".cart-dropdown").hide();
 $("#cart").hover(function () {
   $(".cart-dropdown").show();
+});
+
+$("#close-cart").click(function () {
+  $(".cart-dropdown").hide();
 });
