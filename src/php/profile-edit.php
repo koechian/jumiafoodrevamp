6d7fac5d970ee5aa6980afd635ceda95c1f9983c
mwicigikinuthia <sean.kinuthia@strathmore.edu>
2022-06-22 12:17:04 +0300

    changes has been made to filling as well as database

--- src/php/profile-edit.php
+++ src/php/profile-edit.php
@@ -1,3 +1,14 @@
+<?php 
+  include "config.php";
+
+//   $firstname = "firstname";
+
+//   $lastname = "lastname";
+
+//   $email = "email";
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,11 +29,11 @@
       <h2>Change your Account Details</h2>
       <form>
           <label id = "firstname" for = "fname"> First name: </label>
-          <input type = "text" id = "fname" name = "fname" value="Sean" ><br>
+          <input type = "text" id = "firstname" name = "firstname" value="<?php echo $_POST["firstname"]; ?>" ><br>
           <label id = "lastname" for = "lname" class="lname"> Last name: </label>
-          <input type = "text" id = "lname" name = "lname" value="Kinuthia"><br>
+          <input type = "text" id = "lastname" name = "lastname" value="<?php echo $_POST["lastname"]; ?>"><br>
           <label id = "emailaddress" for = "email" class="email">Email: </label>
-          <input type = "email" id = "email" name = "email" value ="skinuthia19@gmail.com" readonly><br><br>
+          <input type = "email" id = "email" name = "email" value ="<?php echo $_POST["email"]; ?>" readonly><br><br>
           <button id="change">Change</button><br><br>
           <div id = accept-conditions>
                 <label class="container">Accept Emails on offers and orders.
