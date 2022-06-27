e79b081224a8e07f2b3f1a82f62e17a625c7b4fc
koechian <ianovic8@live.com>
2022-06-27 15:49:55 +0300

    Cart items are now displayed

--- src/js/pages-handler.js
+++ src/js/pages-handler.js
@@ -35,7 +35,23 @@ $(document).ready(function () {
       success: function (response) {
         var cart = JSON.parse(response);
         $("#cart").attr("data-after", cart.length);
+        populateCart(cart);
       },
     });
+  }
+  function populateCart(data) {
+    $(
+      $.each(data, function (key, value) {
+        $("#cart-body").append(
+          "<div class='cart-item'><div id='cart-item-left'><div><span id='item-heading'>" +
+            value.product_name +
+            "</span><br><br><br><span>Price</span><br><br><span>Quantity</span></div><div><div><div><span>" +
+            value.product_price +
+            "</span></div><div><button onclick='$(this).next()[0].stepDown()' ><span class='iconify' data-icon='icon-park-solid:reduce-one' style='color: #f90;' data-width='16' data-height='16'></span></button><input value='1' type='number'><button onclick='$(this).prev()[0].stepUp()' ><span class='iconify' data-icon='fluent:add-circle-16-filled' style='color: #f90;'data-width='20' data-height='20'></span></button></div></div></div></div><div id='cart-item-right'><div><img class='cart-img' src='" +
+            value.product_image +
+            "'></div><div><button class='remove-button' id='remove-button'><span class='iconify' data-icon='bi:trash'style='color: rgb(99, 48, 26);' data-width='20' data-height='20'></span></button><span>Remove</span></div></div></div>"
+        );
+      })
+    );
   }
 });
