71473429a232d82c49e66a19378d4f7e97394e4e
mwicigikinuthia <sean.kinuthia@strathmore.edu>
2022-06-28 12:10:23 +0300

    Update profile-edit.php

--- src/php/profile-edit.php
+++ src/php/profile-edit.php
@@ -10,6 +10,16 @@ $sql = "SELECT * FROM users WHERE id='$user'";
 $res = mysqli_query($conn, $sql);
 $result = mysqli_fetch_assoc($res);
 
+if (isset($_POST['Edit'])) {
+    "UPDATE users SET firstname =['?'] WHERE id='$user'";
+}
+
+
+if (isset($_POST['logout'])) {
+    session_destroy();
+    header("Location:../../index.php");
+    exit;
+}
 
 ?>
 <!DOCTYPE html>
@@ -49,20 +59,21 @@ $result = mysqli_fetch_assoc($res);
         </div>
     </header>
     <h2>Change your Account Details</h2>
-    <form>
+    <form method = "POST">
         <label id="firstname" for="fname"> First name: </label>
         <input type="text" id="firstname" name="firstname" value="<?php echo $result['firstname']; ?>">
         <label id="lastname" for="lname" class="lname"> Last name: </label>
         <input type="text" id="lastname" name="lastname" value="<?php echo $result["lastname"]; ?>"><br>
         <label id="emailaddress" for="email" class="email">Email: </label>
         <input type="email" id="email" name="email" value="<?php echo $result["email"]; ?>" readonly><br><br>
-        <button id="change">Change</button><br><br>
         <div id=accept-conditions>
             <label class="container">Accept Emails on offers and orders.
                 <input type="checkbox">
                 <span class="checkmark"></span>
             </label>
         </div>
+        <input type="submit" class="button" id="Edit" name="Edit" value="Edit">
+        <input type="submit" class="button" id="logout" name="logout" value="Log Out">
 
     </form>
 </body>
