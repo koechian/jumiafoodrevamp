9c2216446f7541a8636136f4560e3ee2e544f521
koechian <ianovic8@live.com>
2022-10-20 21:15:02 +0300

    database connection

--- src/php/config.php
+++ src/php/config.php
@@ -3,4 +3,4 @@ $servername = "127.0.0.1";
 $username = "root";
 $password = "database";
 $dbname = "jumia_food";
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname) or die("Unable to connect to '$dbname'");
