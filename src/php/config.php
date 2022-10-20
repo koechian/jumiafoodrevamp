fbe99123d66165c6cca6aed3a6c94d6428221063
koechian <ianovic8@live.com>
2022-10-20 22:14:15 +0300

    asc

--- src/php/config.php
+++ src/php/config.php
@@ -1,6 +1,6 @@
 <?php
 $hostname = "127.0.0.1";
 $username = "root";
-$password = "database";
+$password = "";
 $database = "jumia_food";
 $conn = new mysqli($hostname, $username, $password, $database) or die("Unable to connect to '$database'");
