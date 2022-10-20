ef8591984034c5029fc6c199addcd0fca1e86b01
koechian <ianovic8@live.com>
2022-10-20 22:43:50 +0300

    sax

--- src/php/config.php
+++ src/php/config.php
@@ -3,4 +3,5 @@ $hostname = "127.0.0.1";
 $username = "guest";
 $password = "database";
 $database = "jumia_food";
-$conn = new mysqli($hostname, $username, $password, $database) or die("Unable to connect to '$database'");
+$port = 3306;
+$conn = new mysqli($hostname, $username, $password, $database,$port) or die("Unable to connect to '$database'");
