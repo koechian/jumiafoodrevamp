2b1bc3395cac5bd62f5911836ea152a0969b6c0b
koechian <ianovic8@live.com>
2022-10-20 23:12:45 +0300

    pass

--- src/php/config.php
+++ src/php/config.php
@@ -1,7 +1,6 @@
 <?php
 $hostname = "localhost";
 $username = "guest";
-$password = "database";
+$password = "*A9D467528C52CF9DD63A2168DBE51A8241160241";
 $database = "jumia_food";
-$port = 3306;
-$conn = new mysqli($hostname, $username, $password, $database,$port) or die("Unable to connect to '$database'");
+$conn = new mysqli($hostname, $username, $password, $database) or die("Unable to connect to '$database'");
