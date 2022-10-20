37bb5ff22c9865504990a81579090417c6338371
koechian <ianovic8@live.com>
2022-10-20 23:32:09 +0300

    password updates

--- src/php/config.php
+++ src/php/config.php
@@ -1,6 +1,6 @@
 <?php
 $hostname = "sq11.freemysqlhosting.net";
 $username = "sql11528063";
-$password = "ltsfyi2dfG";
+$password = "Itsfyi2dfG";
 $database = "sql11528063";
 $conn = new mysqli($hostname, $username, $password, $database) or die("Unable to connect to '$database'");
